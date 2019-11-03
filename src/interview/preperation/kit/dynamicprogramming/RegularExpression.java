bb9dfe6557745660cd2a810a70eae79b5cd36383
Manivannan Ramasamy <manivannan.belfazt@gmail.com>
2019-11-03 21:47:37 +0530

    Regex matching

--- src/interview/preperation/kit/dynamicprogramming/RegularExpression.java
+++ src/interview/preperation/kit/dynamicprogramming/RegularExpression.java
@@ -9,6 +9,8 @@ public class RegularExpression {
 		System.out.println(regexMatching("xaaac", "xa*.c"));
 		System.out.println(regexMatching("xbyc", "xa*b.c"));
 		System.out.println(regexMatching("xaayc", "xa*b.c"));
+		System.out.println(regexMatching("ably", "a*b.*y"));
+		System.out.println(regexMatching("ablmy", "a*b.*y"));
 		
 	}
 	
@@ -16,18 +18,22 @@ public class RegularExpression {
 		
 		boolean T[][] = new boolean[st.length() + 1][regex.length() + 1];
 		
-		for(int i = 0; i < st.length() + 1; i++) {
-			for(int j = 0; j < regex.length() + 1; j++) {
-				if(i==0 && j==0) {
-					T[i][j] = true;
-					continue;
-				}else if(i == 0) {
-					T[i][j] = false; 
-					continue;
-				}else if(j == 0) {
-					T[i][j] = false;
-					continue;
-				}
+		for(int i = 0 ; i < st.length() + 1 ; i++) {
+			T[i][0] = false;
+		}
+		
+		T[0][0] = true;
+		
+		for(int i = 1; i < regex.length() + 1; i++) {
+			if(regex.charAt(i - 1)=='*') {
+				T[0][i] = T[0][i - 2];
+			}else {
+				T[0][i] = false; 
+			}
+		}
+		
+		for(int i = 1; i < st.length() + 1; i++) {
+			for(int j = 1; j < regex.length() + 1; j++) {
 				
 				if(st.charAt(i - 1) == regex.charAt(j - 1) || regex.charAt(j - 1) == '.') {
 					T[i][j] = T[i - 1][j - 1]; 
