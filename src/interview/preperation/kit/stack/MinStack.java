5da82801884f3a45d00ec113acfc447fbbe99fa4
Manivannan Ramasamy <manivannan.belfazt@gmail.com>
2020-10-06 09:58:19 +0530

    minStack

---
+++ src/interview/preperation/kit/stack/MinStack.java
@@ -0,0 +1,91 @@
+package interview.preperation.kit.stack;
+
+import java.util.Stack;
+
+/*
+Design a stack that supports push, pop, top, and retrieving the minimum 
+element in constant time.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+getMin() -- Retrieve the minimum element in the stack.
+ 
+
+Example 1:
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+*/
+
+class MinStack {
+    
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<Integer>();
+        minStack = new Stack<Integer>();
+    }
+    
+    public void push(int x) {
+        stack.push(x);
+        if(minStack.isEmpty() || x < minStack.peek()){
+            minStack.push(x);
+        }else{
+            minStack.push(minStack.peek());
+        }
+    }
+    
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+    	if(minStack.isEmpty()) return Integer.MIN_VALUE;
+        return minStack.peek();
+    }
+    
+    public static void main(String args[]) {
+    	MinStack minStack = new MinStack();
+    	minStack.push(5);
+    	System.out.println(minStack.getMin());
+    	minStack.push(2);
+    	minStack.pop();
+    	minStack.push(3);
+    	minStack.push(1);
+    	minStack.push(6);
+    	minStack.push(1);
+    	minStack.pop();
+    	System.out.println(minStack.getMin());
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
