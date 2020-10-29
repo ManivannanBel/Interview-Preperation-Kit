2a4a93dc45f45aa791f3bb0deef109161676f103
Manivannan Ramasamy <manivannan.belfazt@gmail.com>
2020-10-29 07:36:22 +0530

    Kth symbol in grammer

--- src/interview/preperation/kit/misc/KthSymbolInGrammer.java
+++ src/interview/preperation/kit/misc/KthSymbolInGrammer.java
@@ -30,11 +30,15 @@ public class KthSymbolInGrammer {
 	 * If you notice the pattern,
 	 * Eg : N = 4, K = 5 => 01101001
 	 * We can see that the second half is the inverse of the first half of the pattern
+	 * 
 	 * if K > half of the length of the pattern,
 	 *  reduce K by half of the pattern size
 	 * 	We reduce the pattern length to half, and count the flips
 	 * or if K < half of the length of pattern,
 	 *  We reduce the pattern length to half, and count the flips
+	 * Do the above until K > 2
+	 * decrement k to get to 1 or 0
+	 * if flips are odd, then flip the k value
 	 */ 
 	public static int kthGrammar(int N, int K) {
         long noOfBits = (long)Math.pow(2, N - 1);
